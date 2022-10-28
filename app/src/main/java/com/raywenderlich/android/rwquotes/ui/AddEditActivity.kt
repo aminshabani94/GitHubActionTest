391aea63018544e32b625327dbb243061b1d3edc
ASN <aminshabani.workmail@gmail.com>
2022-10-28 21:32:20 +0330

    feature test

--- app/src/main/java/com/raywenderlich/android/rwquotes/ui/AddEditActivity.kt
+++ app/src/main/java/com/raywenderlich/android/rwquotes/ui/AddEditActivity.kt
@@ -62,7 +62,7 @@ class AddEditActivity : AppCompatActivity() {
       addEditBinding.editTextQuoteAuthor.setText(intent.getStringExtra(EXTRA_AUTHOR))
       addEditBinding.editTextQuoteDate.setText(intent.getStringExtra(EXTRA_DATE))
     } else {
-      title = "Add new quote"
+      title = "Add new quoteb"
     }
     addEditBinding.buttonSaveQuote.setOnClickListener {
       saveQuote()
